f0093433c72d35ce0961c7bf722f30d34c361791
luiferpalmera <luiferpalmera@gmail.com>
2019-10-05 15:13:19 -0500

    Instalación AdminLte

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,9 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
